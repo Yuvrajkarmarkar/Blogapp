83f4cb08698c413ded784356b53093165fe7477a
Yuvraj <yykarmarkar_b22@ce.vjti.ac.in>
2024-05-04 06:40:28 +0530

    try

--- Blog_frontend/src/pages/Dashbord.jsx
+++ Blog_frontend/src/pages/Dashbord.jsx
@@ -4,6 +4,8 @@ export default function Dashbord() {
   return (
     <div>
       dashboard
+      fdfhadhfoihf
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ducimus praesentium repellat exercitationem provident temporibus dicta! Amet, molestiae deleniti? Molestias laborum explicabo voluptatibus eaque. Architecto ut non exercitationem voluptatibus eius assumenda quibusdam!</p>
       
     </div>
   )
