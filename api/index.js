0651c49b36e0e05b666ef4f62198e7b9c6946314
Yuvraj <yykarmarkar_b22@ce.vjti.ac.in>
2024-06-14 17:07:29 +0530

    api testing

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,8 +15,14 @@ mongoose.connect(process.env.MONGO).then(
 })
 
 const app = express();
+
+app.use(express.json());
+
+
+
 app.listen(3000, () => {
     console.log('mongodb');
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
