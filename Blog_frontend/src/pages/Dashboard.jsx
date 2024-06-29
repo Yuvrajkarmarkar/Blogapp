ef539887148f393714609901323b77ea976a5284
Yuvraj <yykarmarkar_b22@ce.vjti.ac.in>
2024-06-29 18:57:09 +0530

    add side bar for dashboard

--- Blog_frontend/src/pages/Dashboard.jsx
+++ Blog_frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,30 @@
-import {useSelector} from "react-redux"
-import { Outlet,Navigate } from "react-router-dom"
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 
-export default function Dashbord() {
-  const { currentUser } = useSelector((state) => state.user)
-  
 
-  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;
+export default function Dashboard() {
+  const location =useLocation()
+  const  [tab, setTab ] = useState('')
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFormUrl = urlParams.get('tab')
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+    }
+  }, [location.search]
+  );
+
+  return (
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className="md:w-56">
+        {/* sidebar */}
+        <DashSidebar/>
+      </div>
+      {/* profile */}
+      {tab==='profile'&&<DashProfile/>}
+    </div>
+  )
 }
