f7d9945820d0dd1490fca56a0e2464e2528293c7
Yuvraj <yykarmarkar_b22@ce.vjti.ac.in>
2024-07-02 15:40:28 +0530

    delete user backend add

--- api/controls/user.controller.js
+++ api/controls/user.controller.js
@@ -46,3 +46,15 @@ export const updateUser = async (req, res, next) => {
         next(error);
     }
 }
+
+export const deleteUser = async (req, res, next) => { 
+    if (req.user.id!== req.params.userId) {
+        return next(errorHandler(403, 'you are not authorized to delete'));
+    }
+    try {
+        await User.findByIdAndDelete(req.params.userId);
+        res.status(200).json('User has been deleted');
+    } catch (error) {
+        next(error);
+    }
+}
