0d8a13a446410a00f6af21446c25f4a64100cd16
Yuvraj <yykarmarkar_b22@ce.vjti.ac.in>
2024-07-08 14:40:04 +0530

    add api post

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controls/post.controller.js';
+import { create, getPost } from '../controls/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create',verifyToken,create)
-
+router.get('/getpost',getPost)
 
 
 export default router;
