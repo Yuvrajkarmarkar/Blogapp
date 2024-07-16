cf3bbc910f9e7d77f2b62c4c9dccfed9ab99f227
Yuvraj <yykarmarkar_b22@ce.vjti.ac.in>
2024-07-16 19:04:33 +0530

    add search function and page

--- Blog_frontend/src/App.jsx
+++ Blog_frontend/src/App.jsx
@@ -14,6 +14,7 @@ import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/updatePost'
 import PostPage from './pages/PostPage'
 import ScrollToTop from './components/ScrollToTop'
+import Searchpage from './pages/Searchpage'
 
 function Main() {
   return (
@@ -25,6 +26,7 @@ function Main() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Searchpage/>} />
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
--- Blog_frontend/src/components/header.jsx
+++ Blog_frontend/src/components/header.jsx
@@ -1,6 +1,6 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link ,useLocation} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link ,useLocation, useNavigate} from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
 import {FaMoon,FaSun} from 'react-icons/fa'
 import { useSelector,useDispatch } from 'react-redux';
@@ -13,6 +13,18 @@ export default function header() {
     const { currentUser } = useSelector(state => state.user)
     const dispatch = useDispatch();
     const { theme } = useSelector(state => state.theme);
+    const location = useLocation();
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+    
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl ){
+            setSearchTerm(searchTermFromUrl);
+        }
+    },[location.search])
 
      const handleSignout = async () => {
     try {
@@ -28,7 +40,16 @@ export default function header() {
     } catch (error) {
       console.log(error);
     }
-  }
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+        
+     };
 
 
 
@@ -39,9 +60,11 @@ export default function header() {
                   <span className='px-2 py-1 bg-gradient-to-r from-green-500  to-blue-500 rounded-lg text-white'>Yuvraj's</span>
                   Blog
               </Link>
-              <form>
+              <form onSubmit={handleSubmit}>
                   <TextInput type='text' placeholder='
-                  Search...' rightIcon={AiOutlineSearch} className='hidden lg:inline' />
+                  Search...' rightIcon={AiOutlineSearch} className='hidden lg:inline' value={searchTerm}
+                  onChange={(e)=>setSearchTerm(e.target.value)}
+                  />
               </form>
                   <Button className='w-12 h-10 lg:hidden' color='gray'pill>
                       <AiOutlineSearch/>
